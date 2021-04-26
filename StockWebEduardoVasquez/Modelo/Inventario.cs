656575de0e87f110f6777097b0f8f4a99bd17bd9
eduardo-vqz <eduardo.vqz968@gmail.com>
2021-04-26 15:35:08 -0600

    modelos-finalizados

--- StockWebEduardoVasquez/Modelo/Inventario.cs
+++ StockWebEduardoVasquez/Modelo/Inventario.cs
@@ -6,5 +6,22 @@ namespace StockWebEduardoVasquez.Modelo
 {
     class Inventario
     {
+        private int id;
+        private Sucursal_electrodomestico sucursal_electrodomestico;
+        private int cantidad_movimiento;
+        private string movimiento;
+
+        public Inventario(int id, Sucursal_electrodomestico sucursal_electrodomestico, int cantidad_movimiento, string movimiento)
+        {
+            this.id = id;
+            this.sucursal_electrodomestico = sucursal_electrodomestico;
+            this.cantidad_movimiento = cantidad_movimiento;
+            this.movimiento = movimiento;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Cantidad_movimiento { get => cantidad_movimiento; set => cantidad_movimiento = value; }
+        public string Movimiento { get => movimiento; set => movimiento = value; }
+        internal Sucursal_electrodomestico Sucursal_electrodomestico { get => sucursal_electrodomestico; set => sucursal_electrodomestico = value; }
     }
 }
--- StockWebEduardoVasquez/Modelo/Sucursal_electrodomestico.cs
+++ StockWebEduardoVasquez/Modelo/Sucursal_electrodomestico.cs
@@ -6,5 +6,22 @@ namespace StockWebEduardoVasquez.Modelo
 {
     class Sucursal_electrodomestico
     {
+        private int id;
+        private Sucursales sucursales;
+        private Electrodomesticos electrodomesticos;
+        private int cantidad;
+
+        public Sucursal_electrodomestico(int id, Sucursales sucursales, Electrodomesticos electrodomesticos, int cantidad)
+        {
+            this.id = id;
+            this.sucursales = sucursales;
+            this.electrodomesticos = electrodomesticos;
+            this.cantidad = cantidad;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        internal Sucursales Sucursales { get => sucursales; set => sucursales = value; }
+        internal Electrodomesticos Electrodomesticos { get => electrodomesticos; set => electrodomesticos = value; }
     }
 }
